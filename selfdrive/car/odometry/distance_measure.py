9222b0c7283cef9709cfc43349701df303027505
inha-cvl <cvlab.inhauniv@gmail.com>
2023-02-13 19:30:06 +0900

    update odom code #21

--- selfdrive/car/odometry/distance_measure.py
+++ selfdrive/car/odometry/distance_measure.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 FRONT_TREAD = 1.638 # 1,638mm
 REAR_TREAD = 1.647 #  1,647mm
-WHEEL_DIAMETER = 0.4826 + 0.10 * 2 # 19 * 0.0254 + tire thick * 2 
+WHEEL_DIAMETER = 0.4826 + 0.11*2# 19 * 0.0254 + tire thick * 2 
 ENCODER_RESOLUTION = 46
 KMH2MS = 1 / 3.6
 
@@ -30,86 +30,95 @@ class DistanceMeasurment:
         self.bus = can.ThreadSafeBus(interface='socketcan', channel='can0', bitreate=500000)
         self.db = cantools.database.load_file('../dbc/ioniq/can.dbc')
 
-    def encoder_measure(self):
-        pls_RL = 0
-        pls_RR = 0
-        str_ang = 0
+        self.pose_x = 0
+        self.pose_y = 0
+        self.pose_th = 0
 
-        prev_pls_RL = 0
-        prev_pls_RR = 0
+        self.d = 0
+
+        self.pls_RL = 0
+        self.pls_RR = 0
         
-        pose_x = 0
-        pose_y = 0
-        pose_th = 0
+        self.str_ang = 0
+        
+        self.encoder_cnt_L = 0
+        self.encoder_cnt_R = 0
 
-        d = 0
-        encoder_cnt_L = 0
-        encoder_cnt_R = 0
+        self.prev_pls_RL = -1
+        self.prev_pls_RR = -1
+
+    def encoder_measure(self):
+        
         
         while True:
             try:
                 CAN_data = self.bus.recv(0)
-                print(CAN_data)
+                # print(CAN_data)
                 if CAN_data != None:
                     # print("ok")
                     if CAN_data.arbitration_id == 656:
-                        print(656)
+                        # print(656)
                         data = self.db.decode_message(CAN_data.arbitration_id, CAN_data.data)
-                        str_ang = data['Gway_Steering_Angle']
+                        self.str_ang = data['Gway_Steering_Angle']
                     if CAN_data.arbitration_id == 641:
-                        print(641)
+                        # print(641)
                         data = self.db.decode_message(CAN_data.arbitration_id, CAN_data.data)
+                        if self.prev_pls_RL == -1 or self.prev_pls_RR == -1:
+                            self.prev_pls_RL = data['WHL_PlsRLVal']
+                            self.prev_pls_RR = data['WHL_PlsRRVal']
                         pls_FL = data['WHL_PlsFLVal']
                         pls_FR = data['WHL_PlsFRVal']
-                        pls_RL = data['WHL_PlsRLVal']
-                        pls_RR = data['WHL_PlsRRVal']
-                
-                diff_enc_cnt_L = prev_pls_RL - pls_RL
-                diff_enc_cnt_R = prev_pls_RR - pls_RR
+                        self.pls_RL = data['WHL_PlsRLVal']
+                        self.pls_RR = data['WHL_PlsRRVal']
+                        # print(self.pls_RL, self.pls_RR)
+                        if self.prev_pls_RL != -1 and self.prev_pls_RR != -1:
+                            
+                            diff_enc_cnt_L = self.pls_RL - self.prev_pls_RL
+                            diff_enc_cnt_R = self.pls_RR - self.prev_pls_RR
 
-                diff_enc_cnt_L = diff_enc_cnt_L + 128 if diff_enc_cnt_L < 0 else diff_enc_cnt_L
-                diff_enc_cnt_R = diff_enc_cnt_R + 128 if diff_enc_cnt_R < 0 else diff_enc_cnt_R      
+                            diff_enc_cnt_L = diff_enc_cnt_L + 128 if diff_enc_cnt_L < 0 else diff_enc_cnt_L
+                            diff_enc_cnt_R = diff_enc_cnt_R + 128 if diff_enc_cnt_R < 0 else diff_enc_cnt_R      
+                            # print(diff_enc_cnt_L)
+                            dL = math.pi * WHEEL_DIAMETER / ENCODER_RESOLUTION * diff_enc_cnt_L
+                            dR = math.pi * WHEEL_DIAMETER / ENCODER_RESOLUTION * diff_enc_cnt_R
+                            
+                            dist = (dR + dL) / 2
+                            # self.str_ang = self.str_ang can signal
+                            dth = math.atan((dR - dL) / REAR_TREAD) #+ math.radians(self.str_ang / 13.6) # here is plus alpha ackerman  //// left + right -
+                            # print(dth)
+                            dx = dist*math.cos(dth)
+                            dy = dist*math.sin(dth)
+                            dd=math.sqrt(dx**2 + dy**2)
 
-                dL = math.pi * WHEEL_DIAMETER / ENCODER_RESOLUTION * diff_enc_cnt_L
-                dR = math.pi * WHEEL_DIAMETER / ENCODER_RESOLUTION * diff_enc_cnt_R
-                
-                dist = (dR + dL) / 2
-                # str_ang = str_ang can signal
-                dth = math.atan((dR - dL) / REAR_TREAD) + math.radians(str_ang / 13.73) # here is plus alpha ackerman  //// left + right -
+                            self.pose_x = dx*math.cos(self.pose_th) - dy*math.sin(self.pose_th) + self.pose_x
+                            self.pose_y = dx*math.sin(self.pose_th) + dy*math.cos(self.pose_th) + self.pose_y
+                            self.pose_th += dth
 
-                dx = dist*math.cos(dth)
-                dy = dist*math.sin(dth)
+                            self.d+=dd
+                            if self.d % 10 == 0:
+                                print(self.d," m")
 
-                pose_x = dx*math.cos(pose_th) - dy*math.sin(pose_th) + pose_x
-                pose_y = dx*math.sin(pose_th) + dy*math.cos(pose_th) + pose_y
-                pose_th += dth
+                            self.encoder_cnt_L += diff_enc_cnt_L
+                            self.encoder_cnt_R += diff_enc_cnt_R
 
-                prev_pls_RL = pls_RL
-                prev_pls_RR = pls_RR
+                            if 100 < self.d < 101:
+                                print(self.d," m")
+                                print("encoder cnt_L: ", self.encoder_cnt_L)
+                                print("encoder cnt_R: ", self.encoder_cnt_R)
 
-                dd=math.sqrt(dx**2 + dy**2)
-                d+=dd
-                if d // 10 == 0:
-                    print(d," m")
+                            self.prev_pls_RL = self.pls_RL
+                            self.prev_pls_RR = self.pls_RR
 
-                encoder_cnt_L += diff_enc_cnt_L
-                encoder_cnt_R += diff_enc_cnt_R
-
-                if 100 < d < 101:
-                    print("encoder cnt_L: ", encoder_cnt_L)
-                    print("encoder cnt_R: ", encoder_cnt_R)
-
-                plt.scatter(pose_x, pose_y, color = 'red')
-                plt.pause(0.1)
+                            plt.scatter(self.pose_x, self.pose_y, color = 'red', alpha=0.5)
+                            plt.pause(0.01)
 
             except KeyboardInterrupt:
                         exit(0)
             
-            plt.show()
+        plt.show()
 
 if __name__ == '__main__':
     Activate_Signal_Interrupt_Handler()
 
     DM = DistanceMeasurment()
     DM.encoder_measure()
-    

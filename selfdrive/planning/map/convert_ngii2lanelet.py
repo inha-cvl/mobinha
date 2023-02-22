0931bda5c561b3323ea542cfa0c18960b044877f
maru-jang <zzjune0709@gmail.com>
2023-02-22 14:40:54 +0900

    cleaning ngii2lanelets

--- selfdrive/planning/map/convert_ngii2lanelet.py
+++ selfdrive/planning/map/convert_ngii2lanelet.py
@@ -17,16 +17,19 @@ def main(args):
     with open('%s.json'%(name), 'w', encoding='utf-8') as f:
         json.dump(lanelet.map_data, f, indent="\t")
 
+    with open('%s_ID.json'%(name), 'w', encoding='utf-8') as f:
+        json.dump(lanelet.link_id_data, f, indent="\t")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    name = 'Epitone_HDMap'
+    name = 'songdo+testbed'
 
     parser.add_argument('--ngii_path', type=str, default='./%s'%(name))
     parser.add_argument('--precision', type=float, default=1.0)
-    parser.add_argument('--base_lla', type=tuple, default=(37.2292221592864, 126.76912499027308, 29.18400001525879), help='(lat, lon, alt)')
-    parser.add_argument('--is_utm', type=bool, default=True)
+    parser.add_argument('--base_lla', type=tuple, default=(37.39657805498484, 126.6321430873685,7.369), help='(lat, lon, alt)')
+    parser.add_argument('--is_utm', type=bool, default=False)
 
     args = parser.parse_args()
 

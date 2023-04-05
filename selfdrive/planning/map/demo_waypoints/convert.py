d50e5f89eb560f06939b9e2e8c3c51b23a5817f0
maru-jang <zzjune0709@gmail.com>
2023-04-05 18:41:21 +0900

    demo waypoints modified

--- selfdrive/planning/map/demo_waypoints/convert.py
+++ selfdrive/planning/map/demo_waypoints/convert.py
@@ -3,9 +3,9 @@ import numpy as np
 import pymap3d as pm
 
 # 입력 파일 경로와 파일명
-input_file = "songdo-waypoint.csv"
+input_file = "songdo-demo-waypoints-modified.csv"
 # 출력 파일 경로와 파일명
-output_file = "demo-waypoint-enu.csv"
+output_file = "demo-waypoint-enu-modified.csv"
 # 기준 위도, 경도, 고도
 base_lat, base_lon, base_alt = 37.3888319, 126.6428739, 7.369
 

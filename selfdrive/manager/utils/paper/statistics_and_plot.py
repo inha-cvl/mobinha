7381c8e8fdb9ad77565a18c7847895e53efea605
maru-jang <zzjune0709@gmail.com>
2023-11-20 20:56:49 +0900

    plot update

--- selfdrive/manager/utils/paper/statistics_and_plot.py
+++ selfdrive/manager/utils/paper/statistics_and_plot.py
@@ -5,6 +5,10 @@ class DataPlotter:
     def __init__(self, data):
         self.data = data
 
+    def normalize_timestamps(self, timestamps):
+        start_time = timestamps[0]
+        return [t - start_time for t in timestamps]
+
     def plot(self, y_limits=None):
         if y_limits is None:
             y_limits = {}
@@ -12,28 +16,39 @@ class DataPlotter:
         fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
 
         # 속도 (x 성분만) - km/h 단위로 변환
+        #! TODO XY ^2 sqrt
         velocity_x_kmh = [v[0] * 3.6 for v in self.data['velocity']['data']]
-        axs[0].plot(self.data['velocity']['timestamps'], velocity_x_kmh, label='Velocity X (km/h)')
+        normalized_timestamps = self.normalize_timestamps(self.data['velocity']['timestamps'])
+        axs[0].plot(normalized_timestamps, velocity_x_kmh, label='Velocity (km/h)', color='black')
         axs[0].set_ylabel('Velocity (km/h)')
         axs[0].legend()
         if 'velocity' in y_limits:
             axs[0].set_ylim(y_limits['velocity'])
 
         # 가속도 (x 성분만)
-        axs[1].plot(data['acceleration']['timestamps'], [a[0] for a in data['acceleration']['data']], label='Acceleration X')
+        normalized_timestamps = self.normalize_timestamps(self.data['acceleration']['timestamps'])
+        axs[1].plot(normalized_timestamps, [a[0] for a in data['acceleration']['data']], label='Acceleration X', color='black')
         axs[1].set_ylabel('Acceleration (m/s²)')
         axs[1].legend()
+        if 'acceleration' in y_limits:
+            axs[1].set_ylim(y_limits['acceleration'])
 
         # 저크 (x 성분만)
-        axs[2].plot(data['jerk']['timestamps'], [j[0] for j in data['jerk']['data']], label='Jerk X')
+        normalized_timestamps = self.normalize_timestamps(self.data['jerk']['timestamps'])
+        axs[2].plot(normalized_timestamps, [j[0] for j in data['jerk']['data']], label='Jerk X', color='black')
         axs[2].set_ylabel('Jerk (m/s³)')
         axs[2].legend()
+        if 'jerk' in y_limits:
+            axs[2].set_ylim(y_limits['jerk'])
 
         # 롤
-        axs[3].plot(data['roll']['timestamps'], data['roll']['data'], label='Roll')
+        normalized_timestamps = self.normalize_timestamps(self.data['roll']['timestamps'])
+        axs[3].plot(normalized_timestamps, data['roll']['data'], label='Roll', color='black')
         axs[3].set_ylabel('Roll (degrees)')
         axs[3].set_xlabel('Time (s)')
         axs[3].legend()
+        if 'roll' in y_limits:
+            axs[3].set_ylim(y_limits['roll'])
 
         plt.show()
 

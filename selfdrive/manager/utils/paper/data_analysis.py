d87684a8df02dcd954f3d7e50dafad86939c43cb
maru-jang <zzjune0709@gmail.com>
2023-11-20 20:41:59 +0900

    plot update

--- selfdrive/manager/utils/paper/data_analysis.py
+++ selfdrive/manager/utils/paper/data_analysis.py
@@ -13,11 +13,10 @@ def numpy_to_list(data):
     return data
 
 class PathDataAnalyzer:
-    def __init__(self, map_name, start_point_gt, end_point_gt):
+    def __init__(self, map_name):
         self.map = map_name
         self.gt_coords = self.load_gt_coords()
-        self.start_point_gt = start_point_gt
-        self.end_point_gt = end_point_gt
+        self.gt_yaws = self.load_gt_yaws()
 
     def load_gt_coords(self):
         with open(f'{self.map}_path_data.json', 'r') as file:
@@ -25,116 +24,108 @@ class PathDataAnalyzer:
         x_coords = [point[0] for point in data['global_path']]
         y_coords = [point[1] for point in data['global_path']]
         return list(zip(x_coords, y_coords))
+    
+    def load_gt_yaws(self):
+        with open(f'{self.map}_path_data.json', 'r') as file:
+            data = json.load(file)
+        return data['global_yaw']
+    
+    def find_nearest_veh_time(self, point, veh_data, veh_timestamps):
+        min_distance = float('inf')
+        nearest_time = None
+
+        for data, t in zip(veh_data, veh_timestamps):
+            distance = np.sqrt((data[0] - point[0])**2 + (data[1] - point[1])**2)
+            if distance < min_distance:
+                min_distance = distance
+                nearest_time = t
+
+        return nearest_time
 
     def extract_odom(self, bag_file):
-        v_data, a_data, jerk_data, roll_data, pitch_data, timestamps = [], [], [], [], [], []
+        v_data, a_data, jerk_data, roll_data, pitch_data, veh_data = [], [], [], [], [], []
+        v_timestamps, a_timestamps, jerk_timestamps, roll_timestamps, pitch_timestamps, veh_timestamps = [], [], [], [], [], []
         bag = rosbag.Bag(bag_file)
         for topic, msg, t in bag.read_messages(topics=['/novatel/oem7/odom', '/gps/imu', '/veh_pose', '/novatel/oem7/inspva']):
             if topic == '/novatel/oem7/odom':
                 v_data.append((msg.twist.twist.linear.x, msg.twist.twist.linear.y))
+                v_timestamps.append(t.to_sec())
             if topic == '/gps/imu':
                 a_data.append((msg.linear_acceleration.x, msg.linear_acceleration.y))
-                timestamps.append(msg.header.stamp.to_sec())
+                a_timestamps.append(t.to_sec())
             if topic == '/novatel/oem7/inspva':
                 roll_data.append(msg.roll)
                 pitch_data.append(msg.pitch)
+                roll_timestamps.append(t.to_sec())
+                pitch_timestamps.append(t.to_sec())
+            if topic == '/veh_pose':
+                veh_data.append((msg.x, msg.y, msg.theta))
+                veh_timestamps.append(t.to_sec())
         bag.close()
 
         for i in range(1, len(a_data)):
             delta_acc = np.array(a_data[i]) - np.array(a_data[i - 1])
-            delta_time = timestamps[i] - timestamps[i - 1]
+            delta_time = a_timestamps[i] - a_timestamps[i - 1]
             jerk = delta_acc / delta_time if delta_time > 0 else (0, 0)
             jerk_data.append(jerk)
+            jerk_timestamps.append(a_timestamps[i])  # Jerk의 타임스탬프를 기록
 
-        return v_data, a_data, jerk_data, roll_data, pitch_data, timestamps
+        return {
+                "velocity": (v_data, v_timestamps),
+                "acceleration": (a_data, a_timestamps),
+                "jerk": (jerk_data, jerk_timestamps),
+                "roll": (roll_data, roll_timestamps),
+                "pitch": (pitch_data, pitch_timestamps),
+                "vehicle": (veh_data, veh_timestamps)
+                }
 
-    def find_nearest_index(self, point):
-        min_distance = float('inf')
-        nearest_index = None
-
-        for index, gt_point in enumerate(self.gt_coords):
-            distance = np.sqrt((gt_point[0] - point[0])**2 + (gt_point[1] - point[1])**2)
-            if distance < min_distance:
-                min_distance = distance
-                nearest_index = index
-
-        return nearest_index
-    
-    def find_nearest_time(self, target_time, timestamps):
-        # 가장 작은 시간 차이와 그 인덱스를 초기화
-        min_time_diff = float('inf')
-        nearest_time = None
-
-        # 모든 타임스탬프를 순회하며 가장 가까운 시간을 찾음
-        for time in timestamps:
-            time_diff = abs(time - target_time)
-            if time_diff < min_time_diff:
-                min_time_diff = time_diff
-                nearest_time = time
-
-        return nearest_time
-
-    def process_bag_file(self, bag_file, name):
-        bag_path = f'./{self.map}/{bag_file}'
-        v_data, a_data, jerk_data, roll_data, pitch_data, timestamps = self.extract_odom(bag_path)
-
-        start_time = self.find_nearest_time(self.start_point_gt, timestamps)
-        end_time = self.find_nearest_time(self.end_point_gt, timestamps)
-
-        v_data_extracted = [v for v, t in zip(v_data, timestamps) if start_time <= t <= end_time]
-        a_data_extracted = [a for a, t in zip(a_data, timestamps) if start_time <= t <= end_time]
-        jerk_data_extracted = [j for j, t in zip(jerk_data, timestamps) if start_time <= t <= end_time]
-        roll_data_extracted = [r for r, t in zip(roll_data, timestamps) if start_time <= t <= end_time]
-        pitch_data_extracted = [p for p, t in zip(pitch_data, timestamps) if start_time <= t <= end_time]
-        
-        data_to_save = {
-            "velocity": numpy_to_list(v_data_extracted),
-            "acceleration": numpy_to_list(a_data_extracted),
-            "jerk": numpy_to_list(jerk_data_extracted),
-            "roll": numpy_to_list(roll_data_extracted),
-            "pitch": numpy_to_list(pitch_data_extracted)
-        }
-
-        # start_index = self.find_nearest_index(self.start_point_gt)
-        # end_index = self.find_nearest_index(self.end_point_gt)
-
-        # data_to_save = {
-        #     "velocity": numpy_to_list(v_data[start_index:end_index + 1]),
-        #     "acceleration": numpy_to_list(a_data[start_index:end_index + 1]),
-        #     "jerk": numpy_to_list(jerk_data[start_index:end_index + 1]),
-        #     "roll": numpy_to_list(roll_data[start_index:end_index + 1]),
-        #     "pitch": numpy_to_list(pitch_data[start_index:end_index + 1])
-        # }
-
-        print(len(v_data), len(a_data), len(jerk_data), len(roll_data), len(pitch_data))
-
-        with open(f'./{self.map}/{bag_file}_data_{name}.json', 'w') as json_file:
-            json.dump(data_to_save, json_file, indent=4)
-        
     def process_full_bag_file(self, bag_file):
         bag_path = f'./{self.map}/{bag_file}'
-        v_data, a_data, jerk_data, roll_data, pitch_data, timestamps = self.extract_odom(bag_path)
+        data_dict = self.extract_odom(bag_path)
 
         data_to_save = {
-            "velocity": numpy_to_list(v_data),
-            "acceleration": numpy_to_list(a_data),
-            "jerk": numpy_to_list(jerk_data),
-            "roll": numpy_to_list(roll_data),
-            "pitch": numpy_to_list(pitch_data)
+            "velocity": {"data": numpy_to_list(data_dict["velocity"][0]), "timestamps": numpy_to_list(data_dict["velocity"][1])},
+            "acceleration": {"data": numpy_to_list(data_dict["acceleration"][0]), "timestamps": numpy_to_list(data_dict["acceleration"][1])},
+            "jerk": {"data": numpy_to_list(data_dict["jerk"][0]), "timestamps": numpy_to_list(data_dict["jerk"][1])},
+            "roll": {"data": numpy_to_list(data_dict["roll"][0]), "timestamps": numpy_to_list(data_dict["roll"][1])},
+            "pitch": {"data": numpy_to_list(data_dict["pitch"][0]), "timestamps": numpy_to_list(data_dict["pitch"][1])}
         }
-        print(len(v_data), len(a_data), len(jerk_data), len(roll_data), len(pitch_data))
+
         with open(f'./{self.map}/{bag_file}_data_full.json', 'w') as json_file:
+            json.dump(data_to_save, json_file, indent=4)
+
+    def section_process_bag_file(self, bag_file, start_point, end_point, name):
+        bag_path = f'./{self.map}/{bag_file}'
+        data_dict = self.extract_odom(bag_path)
+        veh_data, veh_timestamps = data_dict["vehicle"]
+
+        # 시작점과 끝점의 타임스탬프 찾기
+        start_time = self.find_nearest_veh_time(start_point, veh_data, veh_timestamps)
+        end_time = self.find_nearest_veh_time(end_point, veh_data, veh_timestamps)
+
+        # 필요한 데이터 추출
+        data_to_save = {}
+        for key, (data, timestamps) in data_dict.items():
+            filtered_data = [d for d, t in zip(data, timestamps) if start_time <= t <= end_time]
+            filtered_timestamps = [t for t in timestamps if start_time <= t <= end_time]
+            data_to_save[key] = {"data": numpy_to_list(filtered_data), "timestamps": numpy_to_list(filtered_timestamps)}
+
+        # JSON 파일로 저장
+        with open(f'./{self.map}/{bag_file}_data_{name}.json', 'w') as json_file:
             json.dump(data_to_save, json_file, indent=4)
 
 
 map = 'kcity'
 
-start_point_gt = (470,1634)
-end_point_gt = (363,1835)
-name = 'curve'
-
-analyzer = PathDataAnalyzer(map, start_point_gt, end_point_gt)
+analyzer = PathDataAnalyzer(map)
 
 for bag_file in ['1-1.bag', '2-1.bag', '4-1.bag', '5-1.bag', '6-1.bag']:
-    analyzer.process_bag_file(bag_file, name)
+    
+    start_point_gt = (470,1634)
+    end_point_gt = (363,1835)
+    name = 'curve'
+    analyzer.section_process_bag_file(bag_file, start_point_gt, end_point_gt, name)
+
+
+
     analyzer.process_full_bag_file(bag_file)
--- selfdrive/manager/utils/paper/statistics_and_plot.py
+++ selfdrive/manager/utils/paper/statistics_and_plot.py
@@ -1,8 +1,52 @@
-import geopandas as gpd
-import pymap3d
 import matplotlib.pyplot as plt
-import utm
-import numpy as np
 import json
-import rosbag
-from matplotlib.lines import Line2D
+
+class DataPlotter:
+    def __init__(self, data):
+        self.data = data
+
+    def plot(self, y_limits=None):
+        if y_limits is None:
+            y_limits = {}
+
+        fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
+
+        # 속도 (x 성분만) - km/h 단위로 변환
+        velocity_x_kmh = [v[0] * 3.6 for v in self.data['velocity']['data']]
+        axs[0].plot(self.data['velocity']['timestamps'], velocity_x_kmh, label='Velocity X (km/h)')
+        axs[0].set_ylabel('Velocity (km/h)')
+        axs[0].legend()
+        if 'velocity' in y_limits:
+            axs[0].set_ylim(y_limits['velocity'])
+
+        # 가속도 (x 성분만)
+        axs[1].plot(data['acceleration']['timestamps'], [a[0] for a in data['acceleration']['data']], label='Acceleration X')
+        axs[1].set_ylabel('Acceleration (m/s²)')
+        axs[1].legend()
+
+        # 저크 (x 성분만)
+        axs[2].plot(data['jerk']['timestamps'], [j[0] for j in data['jerk']['data']], label='Jerk X')
+        axs[2].set_ylabel('Jerk (m/s³)')
+        axs[2].legend()
+
+        # 롤
+        axs[3].plot(data['roll']['timestamps'], data['roll']['data'], label='Roll')
+        axs[3].set_ylabel('Roll (degrees)')
+        axs[3].set_xlabel('Time (s)')
+        axs[3].legend()
+
+        plt.show()
+
+
+def load_data(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+name ='curve'
+map = 'kcity'
+file_name = '1-1.bag'
+file_path = f'./{map}/{file_name}_data_{name}.json'
+data = load_data(file_path)
+plotter = DataPlotter(data)
+plotter.plot(y_limits={'velocity': [0, 40]})

c51fa17b72e4577e4aa0f209fb37bb55637beeb2
maru-jang <zzjune0709@gmail.com>
2024-01-15 14:33:22 +0900

    kcity

--- selfdrive/planning/map/convert_ngii2lanelet.py
+++ selfdrive/planning/map/convert_ngii2lanelet.py
@@ -24,20 +24,20 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    name = 'songdo'
+    name = 'KCity'
 
     parser.add_argument('--ngii_path', type=str, default='./%s'%(name))
     parser.add_argument('--precision', type=float, default=0.5)
-    parser.add_argument('--base_lla', type=tuple, default=(37.3888319, 126.6428739, 7.369), help='(lat, lon, alt)')
-    parser.add_argument('--is_utm', type=bool, default=False)
+    parser.add_argument('--base_lla', type=tuple, default=(37.2292221592864, 126.76912499027308, 29.18400001525879), help='(lat, lon, alt)')
+    parser.add_argument('--is_utm', type=bool, default=True)
 
     args = parser.parse_args()
 
     main(args)
 
-    #k city 37.2292221592864, 126.76912499027308, 29.18400001525879
+    #** KCity 37.2292221592864, 126.76912499027308, 29.18400001525879 utm True L_LinkID
     # songdo 37.39657805498484, 126.6321430873685,7.369
-    # songdo-epitone 37.3888319, 126.6428739, 7.369
+    #** songdo-epitone 37.3888319, 126.6428739, 7.369 utm False L_LinKID
     # songdo-testbed 37.4179788, 126.6140342, 7
     # songdo-campus 37.383378, 126.656798, 7
     # fmtc 37.3656136, 126.7251893, 31.8

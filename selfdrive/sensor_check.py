086732a010503df83085a77f77b9682b9d3b725f
mingu <rbtjr98@gmail.com>
2024-01-30 14:55:00 +0900

    sensor_check update

--- selfdrive/sensor_check.py
+++ selfdrive/sensor_check.py
@@ -3,6 +3,8 @@ from sensor_msgs.msg import PointCloud2, CompressedImage
 from std_msgs.msg import Float32MultiArray, Int8, Int16MultiArray
 import numpy as np
 from novatel_oem7_msgs.msg import BESTPOS, INSPVA
+from geometry_msgs.msg import PoseArray
+from jsk_recognition_msgs.msg import BoundingBoxArray
 
 # Camera & Lidar
 class SensorCheck:
@@ -133,6 +135,7 @@ class CanCheck:
         else:
             return 0
 
+'''
 class PerceptionCheck:
     def __init__(self, topic_name, msg_type):
         self.current_time = rospy.Time.now()
@@ -149,6 +152,7 @@ class PerceptionCheck:
             return self.state
         else:
             return 0
+'''
 
 class PlanningCheck:
     def __init__(self, topic_name, msg_type):
@@ -168,30 +172,97 @@ class PlanningCheck:
             return 1
         else:
             return 0
+            
+class ObjectCheck:
+    def __init__(self, topic_name, msg_type, hz_thresh):
+        self.current_time = rospy.Time.now()
+        self.last_time = rospy.Time.now()
+        self.message = ''
+        self.message_count = 0
+        self.state = ''
+        self.hz = 0.0
+        self.hz_thresh = hz_thresh
+
+        rospy.Subscriber(topic_name, msg_type, self.topic_callback)
+
+    def topic_callback(self, msg):
+        self.current_time = rospy.Time.now()
+        self.message = msg.poses
+        self.message_count += 1
+
+    def get_hz(self):
+        if self.current_time != self.last_time:
+            self.hz = self.message_count / (self.current_time - self.last_time).to_sec()
+            self.message_count = 0
+            self.last_time = self.current_time
+        else:
+            self.hz = 0.0
+        return self.hz
+    
+    def check(self):
+        if self.get_hz() > self.hz_thresh:
+            return 1
+        else:
+            return 0
+
+class ClusterCheck:
+    def __init__(self, topic_name, msg_type, hz_thresh):
+        self.current_time = rospy.Time.now()
+        self.last_time = rospy.Time.now()
+        self.message = ''
+        self.message_count = 0
+        self.state = ''
+        self.hz = 0.0
+        self.hz_thresh = hz_thresh
+
+        rospy.Subscriber(topic_name, msg_type, self.topic_callback)
+
+    def topic_callback(self, msg):
+        self.current_time = rospy.Time.now()
+        self.message = msg.boxes
+        self.message_count += 1
+
+    def get_hz(self):
+        if self.current_time != self.last_time: #and len(self.message) != 0:
+            self.hz = self.message_count / (self.current_time - self.last_time).to_sec()
+            self.message_count = 0
+            self.last_time = self.current_time
+        else:
+            self.hz = 0.0
+        return self.hz
+    
+    def check(self):
+        if self.get_hz() > self.hz_thresh:
+            return 1
+        else:
+            return 0
+
        
 def main():
     rospy.init_node('sensor_diagnostics')
-    pub = rospy.Publisher('/sensor_check', Int16MultiArray, queue_size=10)
+    pub = rospy.Publisher('sensor_check', Int16MultiArray, queue_size=10)
     
     cam = SensorCheck('/gmsl_camera/dev/video0/compressed', CompressedImage, 20)
-    lidar = SensorCheck('/ground_removed_cloud', PointCloud2, 3)
+    lidar = SensorCheck('/ground_removed_cloud', PointCloud2, 6)
     gps = GPSCheck('/novatel/oem7/bestpos', BESTPOS, 7, 1.0, 1.0)
     ins = INSCheck('/novatel/oem7/inspva', INSPVA, 40)
     can = CanCheck('/mobinha/car/gateway_state',Int8)
-    perception = PerceptionCheck('/mobinha/perception_state', Int8)
+    #perception = PerceptionCheck('/mobinha/perception_state', Int8)
     planning = PlanningCheck('/mobinha/planning_state', Int16MultiArray)
+    object = ObjectCheck('/mobinha/perception/cameraW/bounding_box', PoseArray, 10)
+    cluster = ClusterCheck('/mobinha/perception/lidar/track_box', BoundingBoxArray, 5)
 
-    
     
     sensor_check = Int16MultiArray()
 
     while not rospy.is_shutdown():
-        sensor_check.data = [cam.check(), lidar.check(), gps.check(), ins.check(), can.check(), perception.check(), planning.check()]
+        
+        # camera,lidar, gps, ins, can, perception, planning
+        sensor_check.data = [cam.check(), lidar.check(), gps.check(), ins.check(), can.check(), 
+                             int(object.check() == cluster.check() == 1), planning.check()]
         pub.publish(sensor_check)
-
-        rospy.sleep(0.2)  # 5fps
+        rospy.sleep(0.2)  # 1초마다 출력
         
     
 if __name__ == '__main__':
     main()
-

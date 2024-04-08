7237048246ea4b1e21d507cc87de788dfef55da0
leejaejun1224 <leejaejun122@naver.com>
2024-04-08 16:19:23 +0900

    gosilsil

--- selfdrive/perception/obstacle_detector.py
+++ selfdrive/perception/obstacle_detector.py
@@ -124,8 +124,13 @@ class ObstacleDetector:
         self.morai_ego_v = msg.orientation.w
 
     def get_lidar_objects(self, local_point, car_idx):
-        dx = self.CS.position.x - self.morai_local_point[0] if self.is_morai else 0
-        dy = self.CS.position.y - self.morai_local_point[1] if self.is_morai else 0
+        
+        # dx = self.CS.position.x - self.morai_local_point[0] if self.is_morai else 0
+        # dy = self.CS.position.y - self.morai_local_point[1] if self.is_morai else 0
+
+        ## for gosilsil in morai
+        dx = 0
+        dy = 0
 
         viz_obstacle = []
         obstacle_sd = []

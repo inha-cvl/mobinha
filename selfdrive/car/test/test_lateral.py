2c7daee66902f5fb917ac97ec091111848afb3e7
jourmain <myung991209@naver.com>
2024-05-17 02:25:22 +0900

    test again

--- selfdrive/car/test/test_lateral.py
+++ selfdrive/car/test/test_lateral.py
@@ -211,11 +211,11 @@ class IONIQ:
                 self.steer = int(self.limit_steer_change(min(max(int(wheel_angle*13.5), -threshold), threshold)))
                 print(self.steer)
                 print(f"v : {self.current_v}, pos: {self.position[0]}. {self.position[1]}, heading: {self.yaw}, target: {lx}, {ly}, idx: {self.idx}")
-                # print(f"v : {self.current_v:.2f}, pos: {self.position[0]:.2f}. {self.position[1]:.2f}, heading: {self.yaw:.2f}, target: {lx:.2f}, {ly:.2f}, idx: {self.idx}")
                 if abs(self.steer - self.prev_steer) > 100:
+                    print("error occurred")
+                    exit(0)
                     # print(f"v : {self.prev_current_v:.2f}, pos: {self.prev_position[0]:.2f}. {self.prev_position[1]:.2f}, heading: {self.prev_yaw:.2f}, target: {lx:.2f}, {ly:.2f}, idx: {self.prev_idx}")
                     # print(f"v : {self.current_v:.2f}, pos: {self.position[0]:.2f}. {self.position[1]:.2f}, heading: {self.yaw:.2f}, target: {lx:.2f}, {ly:.2f}, idx: {self.idx}")
-                    exit(0)
                 self.prev_current_v = self.current_v
                 self.prev_position = self.position
                 self.prev_yaw = self.yaw
@@ -223,7 +223,7 @@ class IONIQ:
                 self.prev_idx = self.idx
                 # print(self.steer)
                 time.sleep(0.01)
-                '''
+                ''' ## 1
                 v : 1.85, pos: -20.17. 27.63, heading: -144.85, target: -23.78, 22.88, idx: 82
                 243
                 v : 1.85, pos: -20.25. 27.57, heading: -144.26, target: -23.78, 22.88, idx: 82
